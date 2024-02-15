54bbe2ff9e6f8ade042f567028f4aba514df2693
Fritz Lin <uxfritz@163.com>
2024-02-15 23:03:20 +0800

    fix git clean detect inconsistency

--- pull.go
+++ pull.go
@@ -10,14 +10,14 @@ func pull(dir string) error {
 	timestamp := time.Now().Format("2023-12-17_16:30:05.000")
 	sh := fmt.Sprintf(`
 		cd %q
-		not_clean=$(git status --porcelain)
+		not_clean=$(if [ -n "$(git status --porcelain)" ]; then echo true; else echo false; fi)
 		dt=%q
-		if [ "$not_clean" ]
+		if [ $not_clean = true ]
 		then
 			git stash push -u -m "$dt"
 		fi
 		git pull --rebase
-		if [ "$not_clean" ]
+		if [ $not_clean = true ]
 		then
 			git stash pop
 		fi
